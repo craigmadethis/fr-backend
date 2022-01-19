26054061c697ea774762aaef34f1895a35aa133c
Craig Shewry <craigshewry@gmail.com>
2022-01-19 17:52:13 +0000

    pls work

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,22 +1,25 @@
-const parse = require('pg-connection-string').parse;
-    const config = parse(process.env.DATABASE_URL);
-    module.exports = ({ env }) => ({
-      connection: {
-        client: 'postgres',
-        connection: {
-          host: config.host,
-          port: config.port,
-          database: config.database,
-          user: config.user,
-          password: config.password,
-    //add this line
-            ssl: {
-              rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
-            },     
+const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
+
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "postgres",
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
+        ssl: {
+          rejectUnauthorized: false,
+        },
       },
-      // add this line
       options: {
-        ssl: env.bool('DATABASE_SSL', false),
+        ssl: true,
       },
     },
-  });
+  },
+});
